38f582e5a53a02ca43f43bce605ad8ebdc447353
atmughra <atmughra@ncsu.edu>
2019-04-02 20:15:05 -0400

    add bloomfilter

---
+++ 00_Graph_OpenMP/include/utils/bloomfilter.h
@@ -0,0 +1,6 @@
+#ifndef BLOOMFILTER_H
+#define BLOOMFILTER_H
+
+
+
+#endif
---
+++ 00_Graph_OpenMP/src/tests/test_bloomfilter.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+
+#include "bloomfilter.h"
+
+int numThreads;
+
+int main(int argc, char *argv[])
+{
+
+	printf("%s\n", "Test bloomfilter here" );
+	return 0;	
+
+}
