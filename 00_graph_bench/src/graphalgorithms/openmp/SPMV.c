2939a0d35d45ca396e660a23f13c14d2a8ef80b5
atmughra <atmughra@ncsu.edu>
2019-06-01 01:33:53 -0400

    added SPMV fixed point Grid implemenation

--- 00_graph_bench/src/graphalgorithms/openmp/SPMV.c
+++ 00_graph_bench/src/graphalgorithms/openmp/SPMV.c
@@ -351,9 +351,226 @@ struct SPMVStats *SPMVPushColumnGraphGrid( __u32 iterations, struct GraphGrid *g
 struct SPMVStats *SPMVPullRowFixedPointGraphGrid( __u32 iterations, struct GraphGrid *graph)
 {
 
+    __u32 v;
+    double sum = 0.0;
+
+    __u32 totalPartitions  = graph->grid->num_partitions;
+
+    struct SPMVStats *stats = newSPMVStatsGraphGrid(graph);
+    struct Timer *timer = (struct Timer *) malloc(sizeof(struct Timer));
+    struct Timer *timer_inner = (struct Timer *) malloc(sizeof(struct Timer));
+
+    __u64 *vector_input = (__u64 *) my_malloc(graph->num_vertices * sizeof(__u64));
+    __u64 *vector_output = (__u64 *) my_malloc(graph->num_vertices * sizeof(__u64));
+
+    printf(" -----------------------------------------------------\n");
+    printf("| %-51s | \n", "Starting SPMV-Row Fixed-Point");
+    printf(" -----------------------------------------------------\n");
+    printf("| %-21s | %-27s | \n", "Iteration", "Time (S)");
+    printf(" -----------------------------------------------------\n");
+
+    //assume any vector input for benchamrking purpose.
+    #pragma omp parallel for
+    for(v = 0; v < graph->num_vertices; v++)
+    {
+        if(graph->grid->out_degree[v])
+            stats->vector_input[v] =  (1.0f / graph->grid->out_degree[v]);
+        else
+            stats->vector_input[v] = 0.001f;
+    }
+
+    #pragma omp parallel for
+    for(v = 0; v < graph->num_vertices; v++)
+    {
+        vector_input[v] = DoubleToFixed64(stats->vector_input[v]);
+    }
+
+
+    Start(timer);
+    for(stats->iterations = 0; stats->iterations < iterations; stats->iterations++)
+    {
+        Start(timer_inner);
+
+        __u32 i;
+        #pragma omp parallel for private(i) schedule (dynamic,numThreads)
+        for (i = 0; i < totalPartitions; ++i)  // iterate over partitions rowwise
+        {
+            __u32 j;
+            // #pragma omp parallel for private(j)
+            for (j = 0; j < totalPartitions; ++j)
+            {
+                __u32 k;
+                __u32 src;
+                __u32 dest;
+                __u64 weight = DoubleToFixed64(0.0001);
+                struct Partition *partition = &graph->grid->partitions[(i * totalPartitions) + j];
+                for (k = 0; k < partition->num_edges; ++k)
+                {
+                    src  = partition->edgeList->edges_array_src[k];
+                    dest = partition->edgeList->edges_array_dest[k];
+
+#if WEIGHTED
+                    weight = DoubleToFixed64(partition->edgeList->edges_array_weight[k]);
+#endif
+
+                    // #pragma omp atomic update
+                    // __sync_fetch_and_add(&stats->vector_output[dest],(weight * stats->vector_input[src]));
+                    // addAtomicFloat(&stats->vector_output[dest], (weight * stats->vector_input[src])
+
+                    // #pragma omp atomic update
+                    vector_output[src] += MULFixed64V1(weight, vector_input[dest]);
+                }
+            }
+        }
+
+        Stop(timer_inner);
+        printf("| %-21u | %-27f | \n", stats->iterations, Seconds(timer_inner));
+
+    }// end iteration loop
+
+    #pragma omp parallel for
+    for(v = 0; v < graph->num_vertices; v++)
+    {
+        stats->vector_output[v] = Fixed64ToDouble(vector_output[v]);
+    }
+
+
+    #pragma omp parallel for reduction(+:sum)
+    for(v = 0; v < graph->num_vertices; v++)
+    {
+
+        sum += ((int)(stats->vector_output[v] * 10 + .5) / 10.0);
+    }
+
+    Stop(timer);
+    stats->time_total = Seconds(timer);
+
+    printf(" -----------------------------------------------------\n");
+    printf("| %-15s | %-15s | %-15s | \n", "Iterations", "Sum", "Time (S)");
+    printf(" -----------------------------------------------------\n");
+    printf("| %-15u | %-15lf | %-15f | \n", stats->iterations, sum, stats->time_total);
+    printf(" -----------------------------------------------------\n");
+
+
+    free(timer);
+    free(timer_inner);
+    free(vector_output);
+    free(vector_input);
+
+    return stats;
+
+
 }
 struct SPMVStats *SPMVPushColumnFixedPointGraphGrid( __u32 iterations, struct GraphGrid *graph)
 {
+
+     __u32 v;
+    double sum = 0.0;
+
+    __u32 totalPartitions  = graph->grid->num_partitions;
+
+    struct SPMVStats *stats = newSPMVStatsGraphGrid(graph);
+    struct Timer *timer = (struct Timer *) malloc(sizeof(struct Timer));
+    struct Timer *timer_inner = (struct Timer *) malloc(sizeof(struct Timer));
+
+    __u64 *vector_input = (__u64 *) my_malloc(graph->num_vertices * sizeof(__u64));
+    __u64 *vector_output = (__u64 *) my_malloc(graph->num_vertices * sizeof(__u64));
+
+    printf(" -----------------------------------------------------\n");
+    printf("| %-51s | \n", "Starting SPMV-Column Fixed-Point");
+    printf(" -----------------------------------------------------\n");
+    printf("| %-21s | %-27s | \n", "Iteration", "Time (S)");
+    printf(" -----------------------------------------------------\n");
+
+    //assume any vector input for benchamrking purpose.
+    #pragma omp parallel for
+    for(v = 0; v < graph->num_vertices; v++)
+    {
+        if(graph->grid->out_degree[v])
+            stats->vector_input[v] =  (1.0f / graph->grid->out_degree[v]);
+        else
+            stats->vector_input[v] = 0.001f;
+    }
+
+    #pragma omp parallel for
+    for(v = 0; v < graph->num_vertices; v++)
+    {
+        vector_input[v] = DoubleToFixed64(stats->vector_input[v]);
+    }
+
+
+    Start(timer);
+    for(stats->iterations = 0; stats->iterations < iterations; stats->iterations++)
+    {
+        Start(timer_inner);
+
+         __u32 j;
+        // #pragma omp parallel for private(j)
+        for (j = 0; j < totalPartitions; ++j)  // iterate over partitions colwise
+        {
+            __u32 i;
+            #pragma omp parallel for private(i) schedule (dynamic,numThreads)
+            for (i = 0; i < totalPartitions; ++i)
+            {
+                __u32 k;
+                __u32 src;
+                __u32 dest;
+                __u64 weight = DoubleToFixed64(0.0001);
+                struct Partition *partition = &graph->grid->partitions[(i * totalPartitions) + j];
+                for (k = 0; k < partition->num_edges; ++k)
+                {
+                    src  = partition->edgeList->edges_array_src[k];
+                    dest = partition->edgeList->edges_array_dest[k];
+
+#if WEIGHTED
+                    weight = DoubleToFixed64(partition->edgeList->edges_array_weight[k]);
+#endif
+
+                    // #pragma omp atomic update
+                    // __sync_fetch_and_add(&stats->vector_output[dest],(weight * stats->vector_input[src]));
+                    // addAtomicFloat(&stats->vector_output[dest], (weight * stats->vector_input[src])
+
+                    // #pragma omp atomic update
+                    vector_output[src] += MULFixed64V1(weight, vector_input[dest]);
+                }
+            }
+        }
+
+        Stop(timer_inner);
+        printf("| %-21u | %-27f | \n", stats->iterations, Seconds(timer_inner));
+
+    }// end iteration loop
+
+    #pragma omp parallel for
+    for(v = 0; v < graph->num_vertices; v++)
+    {
+        stats->vector_output[v] = Fixed64ToDouble(vector_output[v]);
+    }
+
+
+    #pragma omp parallel for reduction(+:sum)
+    for(v = 0; v < graph->num_vertices; v++)
+    {
+
+        sum += ((int)(stats->vector_output[v] * 10 + .5) / 10.0);
+    }
+
+    Stop(timer);
+    stats->time_total = Seconds(timer);
+
+    printf(" -----------------------------------------------------\n");
+    printf("| %-15s | %-15s | %-15s | \n", "Iterations", "Sum", "Time (S)");
+    printf(" -----------------------------------------------------\n");
+    printf("| %-15u | %-15lf | %-15f | \n", stats->iterations, sum, stats->time_total);
+    printf(" -----------------------------------------------------\n");
+
+
+    free(timer);
+    free(timer_inner);
+    free(vector_output);
+    free(vector_input);
+    
+    return stats;
 
 }
 
