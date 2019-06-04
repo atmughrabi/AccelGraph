c50b2d38451f924e9d59d130c06ae1dcf68cbf5c
atmughra <atmughra@ncsu.edu>
2019-06-04 06:16:24 -0400

    added triangle stats structure

--- 00_graph_bench/src/graphalgorithms/openmp/triangleCount.c
+++ 00_graph_bench/src/graphalgorithms/openmp/triangleCount.c
@@ -19,19 +19,58 @@
 
 
 
-struct TCStats *newTCStatsGraphCSR(struct GraphCSR *graph){
+struct TCStats *newTCStatsGraphCSR(struct GraphCSR *graph)
+{
+
+    struct TCStats *stats = (struct TCStats *) my_malloc(sizeof(struct TCStats));
+
+    stats->counts = 0;
+    stats->num_vertices = graph->num_vertices;
+    stats->time_total = 0.0f;
+
+    return stats;
 
 }
-struct TCStats *newTCStatsGraphGrid(struct GraphGrid *graph){
+struct TCStats *newTCStatsGraphGrid(struct GraphGrid *graph)
+{
+    struct TCStats *stats = (struct TCStats *) my_malloc(sizeof(struct TCStats));
+
+    stats->counts = 0;
+    stats->num_vertices = graph->num_vertices;
+    stats->time_total = 0.0f;
+
+    return stats;
+}
+struct TCStats *newTCStatsGraphAdjArrayList(struct GraphAdjArrayList *graph)
+{
+    struct TCStats *stats = (struct TCStats *) my_malloc(sizeof(struct TCStats));
+
+    stats->counts = 0;
+    stats->num_vertices = graph->num_vertices;
+    stats->time_total = 0.0f;
+
+    return stats;
 
 }
-struct TCStats *newTCStatsGraphAdjArrayList(struct GraphAdjArrayList *graph){
+struct TCStats *newTCStatsGraphAdjLinkedList(struct GraphAdjLinkedList *graph)
+{
+
+    struct TCStats *stats = (struct TCStats *) my_malloc(sizeof(struct TCStats));
+
+    stats->counts = 0;
+    stats->num_vertices = graph->num_vertices;
+    stats->time_total = 0.0f;
+
+    return stats;
 
 }
-struct TCStats *newTCStatsGraphAdjLinkedList(struct GraphAdjLinkedList *graph){
+void freeTCStats(struct TCStats *stats)
+{
 
-}
-void freeTCStats(struct TCStats *stats){
+	if(stats){
+
+		free(stats);
+	}
 
 }
 
@@ -39,13 +78,17 @@ void freeTCStats(struct TCStats *stats){
 // ***************					CSR DataStructure							 **************
 // ********************************************************************************************
 
-struct TCStats *triangleCountGraphCSR(__u32 pushpull, struct GraphCSR *graph){
+struct TCStats *triangleCountGraphCSR(__u32 pushpull, struct GraphCSR *graph)
+{
 
 }
-struct TCStats *triangleCountPullGraphCSR(struct GraphCSR *graph){
+struct TCStats *triangleCountPullGraphCSR(struct GraphCSR *graph)
+{
+  
 
 }
-struct TCStats *triangleCountPushGraphCSR(struct GraphCSR *graph){
+struct TCStats *triangleCountPushGraphCSR(struct GraphCSR *graph)
+{
 
 }
 
@@ -53,13 +96,16 @@ struct TCStats *triangleCountPushGraphCSR(struct GraphCSR *graph){
 // ***************					GRID DataStructure							 **************
 // ********************************************************************************************
 
-struct TCStats *triangleCountGraphGrid(__u32 pushpull, struct GraphGrid *graph){
+struct TCStats *triangleCountGraphGrid(__u32 pushpull, struct GraphGrid *graph)
+{
 
 }
-struct TCStats *triangleCountRowGraphGrid(struct GraphGrid *graph){
+struct TCStats *triangleCountRowGraphGrid(struct GraphGrid *graph)
+{
 
 }
-struct TCStats *triangleCountColumnGraphGrid(struct GraphGrid *graph){
+struct TCStats *triangleCountColumnGraphGrid(struct GraphGrid *graph)
+{
 
 }
 
@@ -67,13 +113,16 @@ struct TCStats *triangleCountColumnGraphGrid(struct GraphGrid *graph){
 // ***************					ArrayList DataStructure					     **************
 // ********************************************************************************************
 
-struct TCStats *triangleCountGraphAdjArrayList(__u32 pushpull, struct GraphAdjArrayList *graph){
+struct TCStats *triangleCountGraphAdjArrayList(__u32 pushpull, struct GraphAdjArrayList *graph)
+{
 
 }
-struct TCStats *triangleCountPullGraphAdjArrayList(struct GraphAdjArrayList *graph){
+struct TCStats *triangleCountPullGraphAdjArrayList(struct GraphAdjArrayList *graph)
+{
 
 }
-struct TCStats *triangleCountPushGraphAdjArrayList(struct GraphAdjArrayList *graph){
+struct TCStats *triangleCountPushGraphAdjArrayList(struct GraphAdjArrayList *graph)
+{
 
 }
 
@@ -81,12 +130,15 @@ struct TCStats *triangleCountPushGraphAdjArrayList(struct GraphAdjArrayList *gra
 // ***************					LinkedList DataStructure					 **************
 // ********************************************************************************************
 
-struct TCStats *triangleCountGraphAdjLinkedList(__u32 pushpull, struct GraphAdjLinkedList *graph){
+struct TCStats *triangleCountGraphAdjLinkedList(__u32 pushpull, struct GraphAdjLinkedList *graph)
+{
 
 }
-struct TCStats *triangleCountPullGraphAdjLinkedList(struct GraphAdjLinkedList *graph){
+struct TCStats *triangleCountPullGraphAdjLinkedList(struct GraphAdjLinkedList *graph)
+{
 
 }
-struct TCStats *triangleCountPushGraphAdjLinkedList(struct GraphAdjLinkedList *graph){
+struct TCStats *triangleCountPushGraphAdjLinkedList(struct GraphAdjLinkedList *graph)
+{
 
 }
