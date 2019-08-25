2f866309072edb58d38765e350d5218f27002877
atmughra <atmughra@ncsu.edu>
2019-08-24 23:38:00 -0400

    add two read port ram/ fix error reporting to the PSL

--- 01_capi_integration/accelerator/rtl/error_control.sv
+++ 01_capi_integration/accelerator/rtl/error_control.sv
@@ -14,16 +14,25 @@ module error_control (
 typedef enum int unsigned {
   ERROR_RESET,
   ERROR_IDLE,
-  MMIO_REQ,
-  WAIT_MMIO_REQ,
-  RESET_REQ
+  ERROR_MMIO_REQ,
+  ERROR_WAIT_MMIO_REQ,
+  ERROR_RESET_REQ,
+  ERROR_REPORT,
+  ERROR_FLAG_CLR
 } error_state;
 
 error_state current_state, next_state;
+logic error_flag;
+logic error_flag_latched;
+
+assign error_flag = |external_errors;
 
 always_ff @(posedge clock or negedge rstn) begin
 	if(~rstn)
-		current_state <= ERROR_RESET;
+		if(error_flag_latched)
+			current_state <= ERROR_REPORT;
+		else
+			current_state <= ERROR_RESET;
 	else
 		current_state <= next_state;
 end // always_ff @(posedge clock)
@@ -32,22 +41,28 @@ always_comb begin
 	next_state = ERROR_IDLE;
 	case (current_state)
 		ERROR_RESET: begin
-		next_state = ERROR_IDLE;
+			next_state = ERROR_IDLE;
 		end 
 		ERROR_IDLE: begin
-			if(|external_errors)
-				next_state = MMIO_REQ;
+			if(error_flag)
+				next_state = ERROR_MMIO_REQ;
 			else
 				next_state = ERROR_IDLE;
 		end 
-		MMIO_REQ: begin
+		ERROR_MMIO_REQ: begin
 			if(report_errors_ack)
-				next_state = RESET_REQ;
+				next_state = ERROR_RESET_REQ;
 			else
-				next_state = MMIO_REQ;
+				next_state = ERROR_MMIO_REQ;
 		end 		
-		RESET_REQ: begin
-			next_state = ERROR_RESET;			
+		ERROR_RESET_REQ: begin
+			next_state = ERROR_REPORT;			
+		end 
+		ERROR_REPORT: begin
+			next_state = ERROR_FLAG_CLR;
+		end
+		ERROR_FLAG_CLR: begin
+			next_state = ERROR_IDLE;
 		end 
 	endcase
 end 
@@ -55,18 +70,26 @@ end
 always_ff @(posedge clock) begin
 	case (current_state)
         ERROR_RESET: begin
-        	report_errors <= external_errors;
-        	reset_error	  <= 1'b1;
+        	report_errors 		  <= 64'b0;
+        	reset_error	  		  <= 1'b1;
+        	error_flag_latched	  <= 1'b0;
 		end 
 		ERROR_IDLE: begin
-			report_errors <= external_errors;
-        	reset_error	  <= 1'b1;
+			report_errors 	   <= external_errors;
+        	reset_error	  	   <= 1'b1;
+        	error_flag_latched <= error_flag;
 		end 
-		MMIO_REQ: begin
+		ERROR_MMIO_REQ: begin
 			
 		end 		
-		RESET_REQ: begin
+		ERROR_RESET_REQ: begin
 			reset_error	  <= 1'b0;
+		end
+		ERROR_REPORT: begin
+			reset_error	  <= 1'b1;
+		end
+		ERROR_FLAG_CLR: begin
+			error_flag_latched	  <= 1'b0;
 		end 
 	endcase
 end
--- 01_capi_integration/accelerator/rtl/ram.sv
+++ 01_capi_integration/accelerator/rtl/ram.sv
@@ -23,3 +23,53 @@ module ram #(
     data_out <= memory[rd_addr];
   end
 endmodule
+
+
+
+
+
+module ram_2xrd #(
+  parameter WIDTH = 64,
+  parameter DEPTH = 32,
+  parameter ADDR_BITS = $clog2(DEPTH)
+)(
+  input  logic                  clock,
+  input  logic                  we,
+  input  logic [0:ADDR_BITS-1]  wr_addr,
+  input  logic [0:WIDTH-1]      data_in,
+  input  logic [0:ADDR_BITS-1]  rd_addr1,
+  output logic [0:WIDTH-1]      data_out1,
+  input  logic [0:ADDR_BITS-1]  rd_addr2,
+  output logic [0:WIDTH-1]      data_out2 
+);
+
+ram #(
+    .WIDTH( WIDTH ),
+    .DEPTH( DEPTH )
+)ram1_instant
+(
+    .clock( clock ),
+    .we( wen ),
+    .wr_addr( wr_addr ),
+    .data_in( wr_data ),
+  
+    .rd_addr( rd_addr1 ),
+    .data_out( rd_data1 )
+);
+
+
+ram #(
+    .WIDTH( WIDTH ),
+    .DEPTH( DEPTH )
+)ram2_instant
+(
+    .clock( clock ),
+    .we( wen ),
+    .wr_addr( wr_addr ),
+    .data_in( wr_data ),
+  
+    .rd_addr( rd_addr2 ),
+    .data_out( rd_data2 )
+);
+
+endmodule
