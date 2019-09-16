58033f84cb63112ebf71a3e8505f4e18fc0c452b
atmughra <atmughra@ncsu.edu>
2019-09-16 12:01:26 -0400

    added simple graph fro testing, vertex job scheduling consecutive requests (first phase)

--- 00_graph_bench/src/tests/test_afu.c
+++ 00_graph_bench/src/tests/test_afu.c
@@ -204,7 +204,8 @@ main (int argc, char **argv)
     arguments.weighted = 0;
     arguments.delta = 1;
     arguments.numThreads = 4;
-    arguments.fnameb = "../03_test_graphs/test/graph.wbin";
+    arguments.fnameb = "../03_test_graphs/v51_e1021/graph.wbin";
+    // arguments.fnameb = "../03_test_graphs/p2p-Gnutella31/graph.wbin";
     arguments.fnameb_format = 1;
     arguments.convert_format = 1;
 
