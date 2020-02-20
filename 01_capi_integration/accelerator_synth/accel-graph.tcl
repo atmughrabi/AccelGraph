7b17457960605846a7c2398914a71d83466aaed8
Abdullah Mughrabi <atmughra@ncsu.edu>
2020-02-20 18:23:56 -0500

    added pagerank image, meets timing

--- 01_capi_integration/accelerator_synth/accel-graph.tcl
+++ 01_capi_integration/accelerator_synth/accel-graph.tcl
@@ -5,15 +5,25 @@ set VERSION   [binary format A24 [exec $LIBCAPI/scripts/version.py]]
 set project_revision accel-graph
 set project_algorithm cu_PageRank_pull
 
-project_new $project_name -overwrite -revision $project_revision
+if { $argc != 1 } {
+	puts "Default Project cu_PageRank_pull"
+	set project_algorithm cu_PageRank_pull
+	} else {
+		puts "SET Project to [lindex $argv 0]"
+		set project_algorithm [lindex $argv 0]
+	}
 
-set_global_assignment -name TOP_LEVEL_ENTITY psl_fpga
+	set project_algorithm cu_PageRank_pull
+
+	project_new $project_name -overwrite -revision $project_revision
+
+	set_global_assignment -name TOP_LEVEL_ENTITY psl_fpga
 
 
-source $LIBCAPI/fpga/common.tcl
-source $LIBCAPI/fpga/ibm_sources.tcl
-source $LIBCAPI/fpga/pins.tcl
-source $LIBCAPI/fpga/build_version.tcl
+	source $LIBCAPI/fpga/common.tcl
+	source $LIBCAPI/fpga/ibm_sources.tcl
+	source $LIBCAPI/fpga/pins.tcl
+	source $LIBCAPI/fpga/build_version.tcl
 
 
 # foreach filename [glob ../accelerator/rtl/*.vhd] {
@@ -25,7 +35,7 @@ source $LIBCAPI/fpga/build_version.tcl
 # }
 
 foreach filename [glob ../accelerator_rtl/afu/*.sv] {
-    set_global_assignment -name SYSTEMVERILOG_FILE $filename
+	set_global_assignment -name SYSTEMVERILOG_FILE $filename
 }
 
 # foreach filename [glob ../accelerator/pkg/*.vhd] {
@@ -37,7 +47,7 @@ foreach filename [glob ../accelerator_rtl/afu/*.sv] {
 # }
 
 foreach filename [glob ../accelerator_rtl/pkg/*.sv] {
-    set_global_assignment -name SYSTEMVERILOG_FILE $filename
+	set_global_assignment -name SYSTEMVERILOG_FILE $filename
 }
 
 # foreach filename [glob ../accelerator/cu/*.vhd] {
@@ -49,9 +59,9 @@ foreach filename [glob ../accelerator_rtl/pkg/*.sv] {
 # }
 
 foreach filename [glob ../accelerator_rtl/cu/$project_algorithm/cu/*.sv] {
-    set_global_assignment -name SYSTEMVERILOG_FILE $filename
+	set_global_assignment -name SYSTEMVERILOG_FILE $filename
 }
 
 foreach filename [glob ../accelerator_rtl/cu/$project_algorithm/pkg/*.sv] {
-    set_global_assignment -name SYSTEMVERILOG_FILE $filename
+	set_global_assignment -name SYSTEMVERILOG_FILE $filename
 }
